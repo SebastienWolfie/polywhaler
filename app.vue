3d3d8a0291a4e999db804dc422d25fea539fcd75
SebastienWolfie <malseasers@gmail.com>
2025-12-18 13:19:44 +0000

    Change

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@ onMounted(() => {
   listenToUserStateChanged()
 })
 
-useHead({
+useHead({ 
   title: "Polywhaler",
   link: [{ rel: 'icon', type: 'image/png', href: '/images/logo.svg' }],
   meta: [{ name: 'description', content: 'PolyWhaler analyzes Polymarket wallets on Polygon. Discover whale trades, wallet scores, performance metrics, risk profiles, and real on-chain trading activity.' }]
