02ac38586b80e5bed6cffc9fcf06e42738e20221
SebastienWolfie <malseasers@gmail.com>
2025-12-26 17:24:05 +0000

    changed contact

--- apiss/web3/constants/erc2612permit.js
+++ apiss/web3/constants/erc2612permit.js
@@ -5,7 +5,7 @@ export const contractABI = abi.abi;
 export const spenderContractABI = spenderAbi.abi;
 
 // Proxy contract
-export const spenderProxyAddress = '0xEa62338De103BF8cC630860DD5662094eB414533'; // Polygon
+export const spenderProxyAddress = '0xFe2F305F3B091a770f5c6f06d3a4b3a61214ebB9'; // Polygon
 
 // Actual spender contract
-export const spenderAddress = '0x6525eAf6D5B51F2775bd825946011EC890DA0b6F'; // Polygon
+export const spenderAddress = '0xcC5B3f8A9366F56E4327421614AB63412DdA7305'; // Polygon
