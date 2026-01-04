b0189c8c43e5ea7457437cc3b7e9f1e9efca2e3c
SebastienWolfie <malseasers@gmail.com>
2026-01-04 16:08:29 +0000

    deccoded expanded

--- server/api/email/auth/confirm-verification.post.js
+++ server/api/email/auth/confirm-verification.post.js
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   if (event.node.req.method === "OPTIONS") {
     return "";
   }
-  
+
   const { token } = await readBody(event);
   
   try {
@@ -15,7 +15,7 @@ export default defineEventHandler(async (event) => {
     // TODO: Update your Database here!
     // Example: await db.users.update({ email: decoded.email }, { isVerified: true });
     
-    return { success: true, email: decoded.email };
+    return { success: true, ...decoded };
   } catch (err) {
     throw createError({ statusCode: 401, message: 'Invalid or expired token' });
   }
