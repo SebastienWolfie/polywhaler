6cd3886d0e598a01c4b3858ef1fee129b5a90208
SebastienWolfie <malseasers@gmail.com>
2026-01-04 16:04:32 +0000

    another cors change

--- server/api/email/auth/confirm-verification.post.js
+++ server/api/email/auth/confirm-verification.post.js
@@ -2,25 +2,10 @@ import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET || 'jnkjnjkjnhbjhbyubyhiuh89h89ui87g6uhbutyv7tby67g87huih89h97ug867g78hiugh78yv5erd546f5td5';
 
 export default defineEventHandler(async (event) => {
-
-handleCors(event, {
-    origin: [
-      'http://localhost:3001', 
-      'https://polyaccountverify.firebaseapp.com'
-    ],
-    methods: ['POST', 'OPTIONS'],
-    allowHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    preflight: {
-      statusCode: 204
-    }
-  });
-
-  // 2. If it's an OPTIONS request, handleCors already sent the response. 
-  // We stop execution here for OPTIONS.
-  if (event.method === 'OPTIONS') {
-    return null;
+  if (event.node.req.method === "OPTIONS") {
+    return "";
   }
+  
   const { token } = await readBody(event);
   
   try {
